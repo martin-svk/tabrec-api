3856d2889c3724cae6debe6a7da6c23339150ae6
Martin Toma <martin.toma.svk@gmail.com>
2014-10-26 17:35:47 +0100

    Add db:migrate deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,14 +42,14 @@ namespace :deploy do
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
-  after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc 'Migrate database'
+  taks :migrate do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :rake, "db:migrate"
     end
   end
+
+  after :publishing, :migrate
+  after :migrate, :restart
 end
