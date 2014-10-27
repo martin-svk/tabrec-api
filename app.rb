dcc4248a0a0b6567736241f5c39fa4b3b1cc02a9
Martin Toma <martin.toma.svk@gmail.com>
2014-10-27 19:14:03 +0100

    Add model declarations.

--- app.rb
+++ app.rb
@@ -1,12 +1,20 @@
+# ======================================================================================================================
+# Require declarations
+# ======================================================================================================================
+
 require 'sinatra/base'
 require 'sinatra/activerecord'
 require 'sinatra/json'
+
+# ======================================================================================================================
+# Main APP
+# ======================================================================================================================
 
 class TabRec < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
   get '/' do
-    'Hello from TabRec API!'
+    json message: 'Hello from TabRec API!'
   end
 
   get '/users/:id' do
@@ -15,6 +23,41 @@ class TabRec < Sinatra::Base
   end
 end
 
+# ======================================================================================================================
+# ActiveRecord models
+# ======================================================================================================================
+
+class Log < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :event
+  belongs_to :advice
+  belongs_to :resolution
+end
+
 class User < ActiveRecord::Base
-  validates_presence_of :experience_level
+  has_many :logs
+
+  validates :experience, presence: true, inclusion: { in: %w(beginner advanced expert) }
+  validates :rec_mode, presence: true, inclusion: { in: %w(interactive semi_interactive aggressive) }
+end
+
+class Event < ActiveRecord::Base
+  has_many :logs
+
+  validates :name, presence: true
+  validates :desc, presence: true
+end
+
+class Advice < ActiveRecord::Base
+  has_many :logs
+
+  validates :name, presence: true
+  validates :desc, presence: true
+end
+
+class Resolution < ActiveRecord::Base
+  has_many :logs
+
+  validates :name, presence: true
+  validates :desc, presence: true
 end
