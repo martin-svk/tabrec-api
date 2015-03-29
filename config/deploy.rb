20ebb8862406d9664ffc69fbeb4d1662e76aad70
Martin Toma <martin.toma.svk@gmail.com>
2015-03-29 15:57:16 +0200

    Add thin specific settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'tabrec-api'
 set :repo_url, 'git@github.com:martin-svk/tabrec-api.git'
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/thin.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -20,7 +20,8 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
+      #execute :touch, release_path.join('tmp/restart.txt')
+      execute :thin, "-C #{release_path.join('config/thin.yml')} restart"
     end
   end
 
